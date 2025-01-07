2552dbd448f027fbd5dc9545f948a2f1d94de99a
Julio Villarruel <julio@Julios-MacBook-Pro.local>
2025-01-06 17:07:31 -0800

    Changes made from UC San Diego

--- hello.c
+++ hello.c
@@ -6,6 +6,7 @@ int main (void)
 {
     printf("Hello World\n\n");
     printf("This is a new piece of code");
-    printf("This is the third edit made to this piece of code.")
-    printf("Hi Julio")
+    printf("This is the third edit made to this piece of code.");
+    printf("Hi Julio");
+    printf("I am making an update to the code from UC San Diego");
 }
