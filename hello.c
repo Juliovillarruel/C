e504bcb5f5c68c576aa0bfd3229f9c0bb870f17b
Julio Villarruel <julio@Julios-MacBook-Pro.local>
2025-01-25 12:29:23 -0800

    This is a new commit

--- hello.c
+++ hello.c
@@ -1,12 +1,10 @@
- #include <stdio.h>
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
 int main (void)
 {
-    printf("Hello World\n\n");
-    printf("This is a new piece of code");
-    printf("This is the third edit made to this piece of code.");
-    printf("Hi Julio");
-    printf("I am making an update to the code from UC San Diego");
+
+printf("Hello world, how are you?\n\n");
+
 }
