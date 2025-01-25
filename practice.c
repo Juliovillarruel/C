571efdb12ba8f2f92d72239ce8725b593f689e81
Julio Villarruel <Juliovillarruel2000@gmail.com>
2025-01-25 13:55:50 -0800

    New

--- practice.c
+++ practice.c
@@ -4,9 +4,5 @@
 
 int main(void)
 {
-    printf("This is my practice page");
-    printf("A new message");
-    printf("A new update bitchx");
-    printf("Can you see this julio?");
-    printf("I think I finally figured it out");
+
 }
