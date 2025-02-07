041087a2323a9c351772af779d6886394a98df50
Julio Villarruel <Juliovillarruel2000@gmail.com>
2025-02-06 16:49:07 -0800

    Updates

--- ai.c
+++ ai.c
@@ -5,4 +5,13 @@
 int main(void)
 {
     printf("Hello how are you\n\n");
+
+    int dataSetNumOne[]= {2,4,5,6,8,10};
+    char names[10] = "Hello";
+
+    printf("%d\n\n", dataSetNumOne[0]);
+    printf("%c\n\n", names[0]);
+
+    
+
 }
