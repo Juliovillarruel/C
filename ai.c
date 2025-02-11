07ca615ce0d7e6847121dfb675512c550014c444
Julio Villarruel <juliovillarruel2000@gmail.com>
2025-02-10 18:38:43 -0800

    Im still so confused omg what the helll can I do?

--- ai.c
+++ ai.c
@@ -9,7 +9,9 @@ int main(void)
     printf("\t\t\t------------------------------------\n");
 
 
-    char firstResponse[100] = "hello"
+    char firstResponse[100] = "jjjjjjuuuuuulllllhelloiiiiiiiiiooooooooooo";
+
+    /*
 
     for (int a = 0; a < 100; a++)
     {
@@ -23,13 +25,43 @@ int main(void)
                     {
                         if (firstResponse[4] == 'o')
                         {
-                            printf("The first word was detected as hello");
+                            printf("The first word was detected as hello\n\n");
+                            break;
                         }
                     }
                 }
             }
         }
-        
+    }
+    */
+
+
+    int b;
+
+    for (int a = 0; a < 100; a++)
+    {
+        if (firstResponse[a] == 'h')
+        {
+            int a = 0;
+            if (firstResponse[a] == 'e')
+            {
+                int a = 0;
+                if (firstResponse[a] == 'l')
+                {
+                    int a = 0;
+                   if (firstResponse[a] == 'l')
+                   {    
+                        int a = 0;
+                        if (firstResponse[a] == 'o')
+                        {
+                            printf("We detected a hello in the entire subset\n\n");
+                        }
+                   }
+                }
+            }
+        }    
+
+    }
 
 
 }
