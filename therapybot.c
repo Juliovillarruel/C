6351006df75e91f26243236cc6a6fb0a4fb596b5
Julio Villarruel <juliovillarruel2000@gmail.com>
2025-02-18 11:07:48 -0800

    This is a new update

---
+++ therapybot.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("\n\n\t\t\t-------------------------- How can I assist you today? --------------------------\n\n");
+
+
+    char sadLibrary[100] = 
+
+    char data[100] = "I just feel so sad all the time";
+
+    for (int a = 0; a < 100; a++)
+    {
+        if (data[a] == 's')
+        {
+            if (data[a + 1] == 'a')
+            {
+                if (data[a + 2] == 'd')
+                {
+
+                }
+                })
+
+}
